7e6b7bd1df369e42f44043dbaeb3b98201637a8f
Balazs <balazs.endresz@gmail.com>
2015-11-06 15:53:44 +0000

    Added merge migration

---
+++ django-verdant/rca/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rca', '0017_merge'),
+        ('rca', '0017_auto_20151106_1550'),
+    ]
+
+    operations = [
+    ]
