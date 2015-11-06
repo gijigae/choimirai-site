28dea3acdfd7423befe13d49d3db8d9c8ff1df45
Balazs <balazs.endresz@gmail.com>
2015-11-06 10:16:48 +0000

    Merge migration

--- django-verdant/rca/migrations/0016_merge.py
+++ django-verdant/rca/migrations/0016_merge.py
@@ -9,6 +9,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('rca', '0014_auto_20151013_1434'),
         ('rca', '0015_merge'),
+        ('rca', '0016_auto_20151106_0938'),
     ]
 
     operations = [
